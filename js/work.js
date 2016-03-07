a481aa7b35175a04c147cd2b3be1c8b28a029971
unknown <cmackm@hotmail.com>
2016-03-06 20:11:46 -0600

    Fixed an issue with dynamic row generating, added custom scrollbar for chrome, smooth scrolling and fixed stuck focus issues on navbar

--- js/work.js
+++ js/work.js
@@ -1,31 +1,10 @@
+
+
 var works = [
-/*"img/macklogo.png' alt='Mack Arquitectos'",
-"http://placehold.it/300x311' alt='Placeholder'", 
-"http://placehold.it/300x311' alt='Placeholder'",
-"http://placehold.it/300x311' alt='Placeholder'",
-"http://placehold.it/300x311' alt='Placeholder'",
-"http://placehold.it/300x311' alt='Placeholder'",
-"http://placehold.it/300x311' alt='Placeholder'",
-"http://placehold.it/300x311' alt='Placeholder'"*/
 {
 	title: "Mack Arquitectos",
 	pic: "img/macklogo.jpg",
 	link: "http://www.guillermomack.com"
-},
-{
-	title: "Placeholder",
-	pic: "http://placehold.it/300x300",
-	link: "#"
-},
-{
-	title: "Placeholder",
-	pic: "http://placehold.it/300x300",
-	link: "#"
-},
-{
-	title: "Placeholder",
-	pic: "http://placehold.it/300x300",
-	link: "#"
 },
 {
 	title: "Placeholder",
@@ -45,14 +24,19 @@ for (var i = 0; i < works.length; i++){
 		"<img class='img-responsive center-block' src='" + works[i].pic + "'>\n" +
 		"</a></div>\n" +
 		"");
-
 }
 
 var size;
 
-$(window).resize(function(){
+function checkwidth(){
 if ($(window).width() <= 768) {size = 2;}
-else { size = 3;}
+else {size = 3;}
+}
+
+checkwidth();
+
+$(window).resize(function(){
+checkwidth();
 });	
 
 var cols = $('.workrows');
@@ -60,3 +44,15 @@ var cols = $('.workrows');
     cols.slice(i, i+size).wrapAll('<div class="row"></div>');
 }
 
+$(document).ready(function(){
+	var $root = $('html, body');
+    $('.navbar-inverse a').click(function() {
+        var href = $.attr(this, 'href');
+        $root.animate({
+            scrollTop: $(href).offset().top
+        }, 500, function () {
+            window.location.hash = href;
+        });
+        return false;
+    });
+ });   
