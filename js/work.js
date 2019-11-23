6582a0a7201b9cd2c42be187e0d384c924f7079f
Kotoriii <cmackm@hotmail.com>
2019-11-23 19:04:43 +0100

    November 2018 update

--- js/work.js
+++ js/work.js
@@ -40,6 +40,33 @@ for (var i = 0; i < works.length; i++){
 		"");
 }
 
+var companies = [
+{
+	pic: "img/companies/comgy.png",
+	link: "https://comgy.io"
+},
+{
+	pic: "img/companies/hyconic.png",
+	link: "#"
+},
+{
+	pic: "img/companies/jobpioneer.png",
+	link: "#"
+},
+{
+	pic: "img/companies/compuccino.png",
+	link: "https://compuccino.com"
+},
+];
+
+for (var i = 0; i < companies.length; i++){
+	$(".companiesrow").append("\n<div class='col-xs-6 col-md-3 companies'>\n" +
+		"<a target='_blank' href='" + companies[i].link +"'>\n" +
+		"<img class='img-responsive center-block company-img' src='" + companies[i].pic + "'>\n" +
+		"</a></div>\n" +
+		"");
+}
+
 /*
 var size;
 
@@ -72,10 +99,3 @@ $(document).ready(function(){
     });
 
  });   
-
-//https://jsfiddle.net/fvmaeqfc/3
-
-//http://holdirbootstrap.de/examples/theme/
-
-//Accordion w/o Bootstrap
-/* http://cssdeck.com/labs/accordion-without-javascript */
